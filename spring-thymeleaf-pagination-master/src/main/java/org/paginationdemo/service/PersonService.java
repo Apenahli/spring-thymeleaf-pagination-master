a1f1cb5f5d05f06202ee8b1150e0ccdc26cbc516
apenahli <apenahli001@gmail.com>
2020-01-27 11:31:55 +0400

    add new project

---
+++ spring-thymeleaf-pagination-master/src/main/java/org/paginationdemo/service/PersonService.java
@@ -0,0 +1,10 @@
+package org.paginationdemo.service;
+
+import org.paginationdemo.domain.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PersonService {
+
+    Page<Person> findAllPageable(Pageable pageable);
+}
